57493a2b15b5bcc4bc01aa9ef206d084a4f72044
kamikunnn <peacelike11@gmail.com>
2026-05-13 17:09:51 +0900

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const fs = require('fs');
 
 const app = express();
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on port ${port}`));
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
